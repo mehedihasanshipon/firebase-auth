cad14fc74f99d6b4414c957b995da8fe93989056
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-03-13 03:08:55 +0600

    Firebase-auth-setup

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAm08GFC5V8jutMllQKB2_hWE0-JW0XfE",
+    authDomain: "ema-john-personal.firebaseapp.com",
+    projectId: "ema-john-personal",
+    storageBucket: "ema-john-personal.appspot.com",
+    messagingSenderId: "825646025495",
+    appId: "1:825646025495:web:ca2332060f463c21c40552"
+  };
+
+  export default firebaseConfig;
